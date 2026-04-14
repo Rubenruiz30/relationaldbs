7ff7b645a7fd501d41c96644f9b72a3106384407
Ruben <davidruizglobal30@gmail.com>
2026-04-14 09:55:16 +0200

    ejercicio 14/04/2026

--- lib/src/main/java/relationaldbs/dao/UserDaoImplementation.java
+++ lib/src/main/java/relationaldbs/dao/UserDaoImplementation.java
@@ -1,5 +1,9 @@
 package relationaldbs.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import relationaldbs.model.User;
@@ -11,13 +15,51 @@ import relationaldbs.model.User;
  *10 abr 2026
  */
 public class UserDaoImplementation implements UserDao {
-
+	
+	   private static final String JDBC_URL =
+	            "jdbc:postgresql://localhost:5432/happy";
+	    private static final String USERNAME = "postgres";
+	    private static final String PASSWORD = "admin";
+	    static String createTableSQL =
+	                "CREATE TABLE IF NOT EXISTS users (" +
+	                "id SERIAL PRIMARY KEY, " +
+	                "username VARCHAR(255), " +
+	                "psw VARCHAR(255), " +
+	                "isVIP BOOLEAN, " +
+	                "balance FLOAT" +
+	                ");";
+	    
 	@Override
 	public boolean insert(User user) {
 		// TODO Auto-generated method stub
+		
+		// The ingredients that we need to do the task
+		/**
+		 * Insert sql
+		 */
 		String insertSQL = "INSERT INTO users (name, password, isVIP, balance) " +
         "VALUES (?, ?, ?, ?)";
-		return false;
+		/**
+		 * create an object of connection to establish
+		 * a network connection with
+		 * our program
+		 */
+		try (// create an object of connection in 
+		 Connection conn = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+		/**
+		 * create an object of preparedStatement which allows
+		 * us to send and execute sqls
+		 */
+				
+				PreparedStatement ps = conn.prepareStatement(insertSQL)) {
+		ps.setString(1, user.getPassword());
+		ps.setString(2, user.getName());
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return false;
 	}
 
 	@Override
