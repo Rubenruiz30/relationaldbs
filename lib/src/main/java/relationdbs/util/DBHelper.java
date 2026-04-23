33d15bb2ea5ba0aaf3dd8a0fbf9ecfe6706c161d
Ruben <davidruizglobal30@gmail.com>
2026-04-23 10:53:10 +0200

    ejercicio 23/04/2026

---
+++ lib/src/main/java/relationdbs/util/DBHelper.java
@@ -0,0 +1,25 @@
+package relationdbs.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBHelper {
+	   private static final String JDBC_URL =
+	            "jdbc:postgresql://localhost:5432/happy";
+	    private static final String USERNAME = "postgres";
+	    private static final String PASSWORD = "admin";
+	    
+	    static Connection getConnection() {
+	   try {
+		return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+	   } catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	   }
+	   return null;
+	    
+	    
+}	  
+}
